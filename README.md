f8a81f279b9b40de128ae4ef89ece3761338a8ed
Roman <rallyshops@gmail.com>
2022-01-05 19:33:05 +0200

    full

--- README.md
+++
@@ -1,1 +0,0 @@
-# advanced_css_4
---
+++ assets/vendors/js/glightbox-video.js
@@ -0,0 +1,6 @@
+const lightbox = GLightbox({
+    'href': 'https://www.youtube.com/watch?v=CxwJrzEdw1U',
+            'type': 'video',
+            'source': 'youtube', //vimeo, youtube or local
+            'width': 900,
+});
